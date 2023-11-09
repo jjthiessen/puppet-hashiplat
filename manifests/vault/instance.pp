7f6c31d72ace1fa71c2fcc0d0ca275f04aa5421e
Jonathan Thiessen <jonathan@wtfast.com>
2023-11-09 19:14:49 +0000

    ...

--- manifests/vault/instance.pp
+++ manifests/vault/instance.pp
@@ -22,7 +22,7 @@ define hashiplat::vault::instance (
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
-    source => "puppet:///modules/${module_name}/vault.service.erb",
+    source => template("puppet:///modules/${module_name}/vault.service.erb"),
   }
 
   ~> service { $service:
