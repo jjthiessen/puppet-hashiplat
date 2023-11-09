1f49b0bbaee6bfc76b503b0a117858f5ae73dfb5
Jonathan Thiessen <jonathan@wtfast.com>
2023-11-09 19:08:17 +0000

    ...

--- manifests/vault.pp
+++ manifests/vault.pp
@@ -5,17 +5,18 @@
 # @example
 #   include hashiplat::vault
 class hashiplat::vault (
-  Optional[String]  $version     = undef,
-  Boolean           $manage_user = true,
-  String            $user        = 'vault',
-  String            $group       = 'vault',
-  String            $config_dir  = '/etc/vault',
-  String            $data_dir    = '/var/lib/vault',
-  String            $region      = $hashiplat::region,
-  String            $tls_ca      = "${::ssldir}/certs/ca.pem",
-  String            $tls_cert    = "${::ssldir}/certs/${trusted['certname']}.pem",
-  String            $tls_key     = "${::ssldir}/private_keys/${trusted['certname']}.pem",
-  Hash[String, Any] $config      = {},
+  Optional[String]  $version        = undef,
+  Boolean           $manage_user    = true,
+  String            $user           = 'vault',
+  String            $group          = 'vault',
+  String            $config_dir     = '/etc/vault',
+  String            $data_dir       = '/var/lib/vault',
+  String            $region         = $hashiplat::region,
+  String            $puppet_ssl_dir = '/etc/puppetlabs/puppet/ssl',
+  String            $tls_ca         = "${puppet_ssl_dir}/certs/ca.pem",
+  String            $tls_cert       = "${puppet_ssl_dir}/certs/${trusted['certname']}.pem",
+  String            $tls_key        = "${puppet_ssl_dir}/private_keys/${trusted['certname']}.pem",
+  Hash[String, Any] $config         = {},
 ) inherits hashiplat {
   if $version =~ String {
     class { 'hashicorp::vault':
